2d01216bb08ac8282f4d8dce93ec4b90febdf30f
junyong <rlawnsdyd530@gmail.com>
2026-04-17 20:39:22 +0900

    chore: Spring Boot 4.0.5로 업그레이드 & 그에 따른 의존성 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.13"
+	kotlin("jvm") version "2.2.21"
+	kotlin("plugin.spring") version "2.2.21"
+	id("org.springframework.boot") version "4.0.5"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("plugin.jpa") version "2.2.21"
 }
 
 group = "com.unicorn"
@@ -23,9 +23,8 @@ dependencies {
 	// Spring Boot starters
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("org.springframework.boot:spring-boot-starter-aspectj")
 
 	// Kotlin support
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
