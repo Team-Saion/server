f0f92f9b74ca37f9126d8df6379987f3652cb38d
junyong <rlawnsdyd530@gmail.com>
2026-04-17 20:38:30 +0900

    remvoe: Redis 및 Image VO 제거

--- src/main/kotlin/com/unicorn/server/infrastructure/adapter/out/redis/exception/RedisKeyNotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package com.unicorn.server.infrastructure.adapter.out.redis.exception
-
-import com.unicorn.server.common.exception.BusinessException
-
-class RedisKeyNotFoundException(key: String) :
-	BusinessException(RedisErrorCode.REDIS_KEY_NOT_FOUND, "key=$key")
--- src/main/kotlin/com/unicorn/server/infrastructure/adapter/out/redis/exception/RedisOperationFailedException.kt
+++
@@ -1,9 +0,0 @@
-package com.unicorn.server.infrastructure.adapter.out.redis.exception
-
-import com.unicorn.server.common.exception.BusinessException
-
-class RedisOperationFailedException(
-	operation: String,
-	key: String,
-	cause: Throwable? = null,
-) : BusinessException(RedisErrorCode.REDIS_OPERATION_FAILED, "operation=$operation, key=$key", cause)
