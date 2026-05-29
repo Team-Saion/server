52a2db5ca966f11882514d468abe43298aa9e1ec
junyong <rlawnsdyd530@gmail.com>
2026-05-30 00:46:49 +0900

    refactor[YAQZ-93]: DuplicateEmailException 메시지 힌트 보완

--- src/main/kotlin/com/unicorn/server/domain/member/exception/DuplicateEmailException.kt
+++ src/main/kotlin/com/unicorn/server/domain/member/exception/DuplicateEmailException.kt
@@ -4,4 +4,4 @@ import com.unicorn.server.common.exception.BusinessException
 
 // DuplicateEmailException - 서비스 내 이메일 중복이 감지될 때 사용한다.
 class DuplicateEmailException(email: String) :
-	BusinessException(MemberErrorCode.DUPLICATE_EMAIL, "email=$email")
+	BusinessException(MemberErrorCode.DUPLICATE_EMAIL, "Duplicated Email=$email")
