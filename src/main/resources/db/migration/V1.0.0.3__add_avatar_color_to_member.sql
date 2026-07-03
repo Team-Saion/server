7a5244eab889a3c3875aa7cacb5e819f6aae8d60
lso5507 <leeseokwoon@naver.com>
2026-07-03 15:11:01 +0900

    fix[SAIO-162]:Member Avatar_color default 명 변경

--- src/main/resources/db/migration/V1.0.0.3__add_avatar_color_to_member.sql
+++ src/main/resources/db/migration/V1.0.0.3__add_avatar_color_to_member.sql
@@ -1,2 +1,3 @@
 -- Member 테이블에 avatar_color 컬럼 추가
-alter table member add column avatar_color varchar(20) not null default 'BLUE';
+alter table member
+    add column avatar_color varchar(20) not null default 'BLUE_200';
