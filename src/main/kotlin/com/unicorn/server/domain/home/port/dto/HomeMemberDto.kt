87e84dad487ffde82776a660ef0c16ceb08526aa
junyong <rlawnsdyd530@gmail.com>
2026-07-18 04:03:21 +0900

    fix[SAIO-164]: isMe 스웨거 me -> isMe로 보이도록 수정

--- src/main/kotlin/com/unicorn/server/domain/home/port/dto/HomeMemberDto.kt
+++ src/main/kotlin/com/unicorn/server/domain/home/port/dto/HomeMemberDto.kt
@@ -1,5 +1,6 @@
 package com.unicorn.server.domain.home.port.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.unicorn.server.domain.member.enums.AvatarColor
 
 data class HomeMemberDto(
@@ -7,6 +8,7 @@ data class HomeMemberDto(
     val nickname: String,
     val avatarColor: AvatarColor,
     val profileImageKey: String?,
+    @get:JsonProperty("isMe")
     val isMe: Boolean,
     val role: String,
 )
